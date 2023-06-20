d729796a7c90bf8834fa0be541c17ee677a72c29
Davidla0 <93866871+Davidla0@users.noreply.github.com>
2023-06-20 16:12:58 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+
+interface Tasks {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number;
+}
 
 function App() {
+  const loadTasks = useRef(() => {})
+  const [tasks, setTasks] = useState<Tasks[]>([]);
+
+  useEffect(() => {
+    loadTasks.current()
+  }, [])
+
+  loadTasks.current = async () => {
+   const task =  await axios('https://jsonplaceholder.typicode.com/todos')  
+   setTasks(task.data)   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tasks-grid">
+      {tasks && tasks.map(task => (
+        <div key={task.id} className='' style={{background: 'red'}}>
+          <h1>{task.title}</h1>
+          <p>{task.userId}</p>
+        </div>
+      ))}
     </div>
   );
 }
